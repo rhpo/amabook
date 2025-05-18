6e50ac774655473c075b6fd7cb2c7ef21e39e921
Ramy Hadid <ramyhadid.js@gmail.com>
2025-05-18 09:46:24 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env['PORT'] || 3000;
+
+app.get('/', (req, res) => {
+
+	res.send("Hello world!");
+	
+});
+
+app.listen(PORT, () => {
+	console.log("Listening on http://localhost:" + PORT);
+});
